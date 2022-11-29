8a338e29bdf75724faf1a0e59e6ccaa9ffca9ee1
407 <407@academicos.cesde.edu.co>
2022-11-29 16:41:52 -0500

    Se crea Proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,83 @@
+import './Home.css'
+
+export function Home(){
+    return(
+        <>
+            <section>
+                <div class="banner">
+
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                            <h1>The Strokes</h1>
+                        <p>Julian Casablancas, vocalista del grupo y Nikolai Fraiture, bajista, eran amigos desde la infancia. En el Instituto Le Rosey, en Suiza, Julian Casablancas (a quien su padre había enviado por problemas de conducta) y Albert Hammond Jr., uno de los guitarristas, se conocieron ya que los dos eran estadounidenses y por lo tanto hablaban el mismo idioma, aunque no eran muy amigos.
+
+                        Gordon Raphael, un pequeño productor de rock de la escena musical de Nueva York, asistió a uno de sus primeros conciertos donde eran teloneros de DragonForce, cuando realmente había ido a ver a la otra banda que tocaba esa velada, como confesó meses después. Raphael dio a Albert su número de teléfono, diciendo que podría grabar una maqueta para ellos. Después de llamarle un par de días más tarde, la banda y Raphael finalmente se reunieron.
+
+                        El EP «The Modern Age» fue lanzado en 2001 desatando una guerra de pujas entre las discográficas, por ser, según ellas, la banda de rock and roll más grande que había surgido en años.​ Posteriormente, recibieron una gran promoción causando una división entre los seguidores del rock. Para algunos de los medios que los promocionaban, The Strokes eran los salvadores del rock.
+                        </p>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/the-strokes-concert.webp?alt=media&token=f7a033f5-3dce-4df9-a1a6-3264b058ee84" class="d-block w-100" alt="concert"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <di><h1>Conciertos</h1></di>
+                            <div class="row">
+                                <div class="col-6 text-start">
+                                <i class="bi bi-vinyl-fill fs-1 icono">
+                                <h3 class="fw-bold text-center">Medellin</h3>
+                                <h3 class="text-center">Nov-30</h3></i>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 text-start">
+                                <i class="bi bi-vinyl-fill fs-1 icono">
+                                <h3 class="fw-bold text-center">Bogota</h3>
+                                <h3 class="text-center">Dic-5</h3></i>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 text-start">
+                                <i class="bi bi-vinyl-fill fs-1 icono">
+                                <h3 class="fw-bold text-center">Cali</h3>
+                                <h3 class="text-center">Dic-10</h3></i>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6 text-start">
+                                <i class="bi bi-vinyl-fill fs-1 icono">
+                                <h3 class="fw-bold text-center">Santa Marta</h3>
+                                <h3 class="text-center">Dic-15</h3></i>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+            <hr/>
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/The-Strokes-The-New-Abnormal.jpg?alt=media&token=8d506a56-4b19-4b74-8f23-09c99a8e0523" alt="" class="img-fluid w-100"/>
+                            
+                        </div>
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/Lollapalooza-thestrokes.webp?alt=media&token=05cf3517-d452-4263-b1dc-40f40ed25e29" alt="" class="img-fluid w-100"/>
+                            
+                        </div>
+                        <div class="col-12 col-md-4">
+
+                            <img src="https://firebasestorage.googleapis.com/v0/b/spotyjv-121a0.appspot.com/o/ciudad-mexico-thestrokes.jpg?alt=media&token=8d899c64-e332-485c-adc1-c29140dcc200" alt="" class="img-fluid w-100"/>
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
